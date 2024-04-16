697fe6ffb421f495b2184cee7a6b2b79ac63ebed
Mohan Keole <mkgit@users.noreply.github.com>
2024-04-16 14:11:20 -0400

    Adding debugging commands

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,5 +23,8 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
+whoami
+pwd
+sleep 2
 ls -l target/${NAME}-${VERSION}.jar
 java -jar target/${NAME}-${VERSION}.jar
