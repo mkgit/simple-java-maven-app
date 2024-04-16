8d65b920f0bac81cd3b672bbfff089f5f1015c69
Mohan Keole <mkgit@users.noreply.github.com>
2024-04-16 14:18:35 -0400

    Update deliver.sh with dbg

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -25,6 +25,6 @@ echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 whoami
 pwd
-ls -l target
+ls -bl target
 ls -l "target/${NAME}-${VERSION}.jar"
 java -jar target/${NAME}-${VERSION}.jar
